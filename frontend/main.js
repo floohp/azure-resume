2b280de1a3ddff55a15a2144a372fccf5cab7268
Helegoeie <helegoeie@Helegoeies-MacBook-Pro.local>
2023-02-07 10:26:44 +0100

    Added to the about me section

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionAPI = 'http://localhost:7071/api/GetResumeCounter';
+const functionAPIUrl = "https://getresumecounterfloohp.azurewebsites.net/api/GetResumeCounter?code=0k6rqjY2RgvqYJ20HV7ACOOP5zmCqvwn3J5s25_2CNXeAzFuZj5vsQ=="
+const localfunctionAPI = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionAPI).then(response => {
+    fetch(functionAPIUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.")
